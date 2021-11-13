282b3c52150cb3c01edb9874ff779991a53a81c2
cristpsantos <pontescr@gmail.com>
2021-11-13 16:24:20 -0300

    DTO

--- backend/src/main/java/com/pontescr/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/pontescr/dscatalog/services/CategoryService.java
@@ -1,11 +1,13 @@
 package com.pontescr.dscatalog.services;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.pontescr.dscatalog.dto.CategoryDTO;
 import com.pontescr.dscatalog.entities.Category;
 import com.pontescr.dscatalog.repositories.CategoryRepository;
 
@@ -16,8 +18,8 @@ public class CategoryService {
 	private CategoryRepository repository;
 	
 	@Transactional(readOnly = true)
-	public List<Category> findAll() {
+	public List<CategoryDTO> findAll() {
 	    List<Category> list = repository.findAll();
-		return list;
+		return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());	
 	}
-}
+} 
