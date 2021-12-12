354d4e04c3d3d2e79336414ac97d0cadaf877b06
cristpsantos <pontescr@gmail.com>
2021-12-12 12:23:49 -0300

    UserUpdateValidator

---
+++ backend/src/main/java/com/pontescr/dscatalog/dto/UserUpdateDTO.java
@@ -0,0 +1,8 @@
+package com.pontescr.dscatalog.dto;
+
+import com.pontescr.dscatalog.services.validation.UserUpdateValid;
+
+@UserUpdateValid
+public class UserUpdateDTO extends UserDTO {
+	private static final long serialVersionUID = 1L;
+}
---
+++ backend/src/main/java/com/pontescr/dscatalog/services/validation/UserUpdateValidator.java
@@ -0,0 +1,54 @@
+package com.pontescr.dscatalog.services.validation;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.servlet.HandlerMapping;
+
+import com.pontescr.dscatalog.dto.UserUpdateDTO;
+import com.pontescr.dscatalog.entities.User;
+import com.pontescr.dscatalog.repositories.UserRepository;
+import com.pontescr.dscatalog.resources.exceptions.FieldMessage;
+
+public class UserUpdateValidator implements ConstraintValidator<UserUpdateValid, UserUpdateDTO> {
+	
+	@Autowired
+	HttpServletRequest request;
+	
+	@Autowired
+	UserRepository repository;
+	
+	@Override
+	public void initialize(UserUpdateValid ann) {
+	}
+
+	@Override
+	public boolean isValid(UserUpdateDTO dto, ConstraintValidatorContext context) {
+		
+		@SuppressWarnings("unchecked")
+		var uriVars = (Map<String, String>) request.getAttribute(HandlerMapping.URI_TEMPLATE_VARIABLES_ATTRIBUTE);
+		
+		long userId = Long.parseLong(uriVars.get("id"));
+		
+		List<FieldMessage> list = new ArrayList<>();
+		
+		User user = repository.findByEmail(dto.getEmail());
+		
+		if(user != null && user.getId() != userId) {
+			list.add(new FieldMessage("Email", "Email já existe"));
+		}
+		
+		for (FieldMessage e : list) {
+			context.disableDefaultConstraintViolation();
+			context.buildConstraintViolationWithTemplate(e.getMessage()).addPropertyNode(e.getFieldName())
+					.addConstraintViolation();
+		}
+		return list.isEmpty();
+	}
+}
