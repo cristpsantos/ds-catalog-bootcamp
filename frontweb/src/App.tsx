e9ed23911137fb3e37bb8afb7e7009dd093e5951
cristpsantos <pontescr@gmail.com>
2022-01-15 23:11:28 -0300

    Routes, ProductCard, ProductPrice and ButtonIcon

--- frontweb/src/App.tsx
+++ frontweb/src/App.tsx
@@ -1,12 +1,12 @@
-import './assets/styles/custom.scss';
+import 'assets/styles/custom.scss';
 import './App.css';
 
-import Home from 'pages/Home';
+import Routes from 'Routes';
 
-const App = () => {
+function App() {
   return (
-      <Home />
+    <Routes />
   );
-};
+}
 
 export default App;
