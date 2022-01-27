09f207c279e23a511b9190412fba4fafdda8e2cf
cristpsantos <pontescr@gmail.com>
2022-01-26 22:06:14 -0300

    Pagination styling

--- frontweb/src/pages/Catalog/index.tsx
+++ frontweb/src/pages/Catalog/index.tsx
@@ -1,3 +1,4 @@
+import Pagination from 'components/Pagination';
 import ProductCard from 'components/ProductCard';
 import { Link } from 'react-router-dom';
 
@@ -58,6 +59,9 @@ const Catalog = () => {
             <ProductCard product={product} />
           </Link>
         </div>
+      </div>
+      <div className="row">
+        <Pagination />
       </div>
     </div>
   );
