b553d2eb2dc11cfdd76f86c956f6eb60c031424c
cristpsantos <pontescr@gmail.com>
2022-03-01 20:04:49 -0300

    Product crud routes

---
+++ frontweb/src/pages/Admin/Products/Form/index.tsx
@@ -0,0 +1,7 @@
+const Form = () => {
+    return(
+        <h1>Tela do Form</h1>
+    );
+}
+
+export default Form;
---
+++ frontweb/src/pages/Admin/Products/List/index.tsx
@@ -0,0 +1,7 @@
+const List = () => {
+    return(
+        <h1>Tela da lista de produtos</h1>
+    );
+}
+
+export default List;
