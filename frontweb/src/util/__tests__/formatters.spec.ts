c080f1bc1b78a44d200406e4fb8bd37871dc0416
Cristiano Pontes <pontescr@gmail.com>
2022-03-23 22:17:17 -0300

    Mocking function with SpyOn

--- frontweb/src/util/__tests__/formatters.spec.ts
+++ frontweb/src/util/__tests__/formatters.spec.ts
@@ -13,16 +13,3 @@ describe('FormatPrice positive numbers', () => {
         expect(result).toEqual("0,10");
     });
 });
-
-describe('FormatPrice non-positive numbers' , () => {
-
-    test('Should format number pt-BR when given 0.0', () => {
-        const result = FormatPrice(0.0);
-        expect(result).toEqual('0,00');
-    });
-
-    test('Should format number pt-BR when given -5.1', () => {
-        const result = FormatPrice(-5.1);
-        expect(result).toEqual('-5,10');
-    });
-});
